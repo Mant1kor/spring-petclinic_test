9c63ac7663dd70df05ecd130b0058daefa9c8785
Mykola Ronik <mykola.ronik@gmail.com>
2022-12-24 16:29:39 +0200

    Added Terraform file

---
+++ main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+#  access_key = "my-access-key"
+#  secret_key = "my-secret-key"
+}
+
+# Create a VPC
+resource "aws_vpc" "project1" {
+  cidr_block = "10.10.0.0/18"
+
+  tags = {
+    Name = "project-vpc"
+  }
+}
+
+# Create subnet
+resource "aws_subnet" "main_subnet" {
+  vpc_id            = aws_vpc.project1.id
+  cidr_block        = "10.10.10.0/24"
+
+  tags = {
+    Name = "project-main"
+  }
+}
+
+# SSH public key
+resource "aws_key_pair" "robot" {
+  key_name   = "robot"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKl01HNCZCoXsxy0XB3v7xPKjQmoeZHry87APvFe7rS+ mykola.ronik@gmail.com"
+}
+
+# Create instance
+resource "aws_instance" "worker1" {
+  ami           = "ami-05ff5eaef6149df49"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.robot.key_name
+
+  tags = {
+    Name = "project-worker1"
+  }
+}
+
+output "instance_dns_name" {
+  value = aws_instance.worker1.public_dns
+}
+
+output "instance_public_ip_address" {
+  value = aws_instance.worker1.public_ip
+}
