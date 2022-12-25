808fbb8b2f9733cded2c9f658c92cb1f5673157a
Mykola Ronik <mykola.ronik@gmail.com>
2022-12-25 14:32:22 +0200

    Added security group rules

--- main.tf
+++ main.tf
@@ -33,17 +33,50 @@ resource "aws_subnet" "main_subnet" {
   }
 }
 
+# Create security group
+resource "aws_security_group" "project1_sec_group" {
+  name        = "allow configuration and access"
+  description = "Allow ssh for configuration and 8080 for access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["213.109.233.0/24"]
+  }
+
+  ingress {
+    description      = "http access"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 # SSH public key
 resource "aws_key_pair" "robot" {
   key_name   = "robot"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKl01HNCZCoXsxy0XB3v7xPKjQmoeZHry87APvFe7rS+ mykola.ronik@gmail.com"
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKl01HNCZCoXsxy0XB3v7xPKjQmoeZHry87APvFe7rS+ mykola.ronik@gmail.com"
 }
 
 # Create instance
 resource "aws_instance" "worker1" {
-  ami           = "ami-05ff5eaef6149df49"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.robot.key_name
+  ami             = "ami-05ff5eaef6149df49"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.robot.key_name
+  security_groups = aws_security_group.project1_sec_group.id
 
   tags = {
     Name = "project-worker1"
